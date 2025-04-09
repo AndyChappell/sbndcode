6f8457a3be2079564c6e904a3339c519efeb2160
AndyChappell <andrew.chappell@warwick.ac.uk>
2025-04-09 09:43:59 -0500

    Add additional metadata variables

--- sbndcode/PandoraValidation/PandoraValidation_module.cc
+++ sbndcode/PandoraValidation/PandoraValidation_module.cc
@@ -22,6 +22,7 @@
 #include "lardataobj/RecoBase/Slice.h"
 #include "lardataobj/RecoBase/PFParticle.h"
 #include "lardataobj/RecoBase/PFParticleMetadata.h"
+#include "lardataobj/RecoBase/Vertex.h"
 
 #include <TTree.h>
 
@@ -52,12 +53,18 @@ class validation::PandoraValidation : public art::EDAnalyzer {
     private:
         void Reset();
 
+        template<typename T>
+        void AddPropertyToVector(const larpandoraobj::PFParticleMetadata::PropertiesMap &properties, const std::string &key, std::vector<T> vector);
+
         TTree *fTree;
         unsigned int fEventID;
         std::string fSliceLabel;
         std::string fPFParticleLabel;
         std::string fTrackScoreKey;
         bool fIsNuSlice;
+        float fNuVertexX;
+        float fNuVertexY;
+        float fNuVertexZ;
         unsigned int fNPFParticle;
         std::vector<float> fTrackScore;
         std::vector<float> fConeChargeConcentration;
@@ -73,6 +80,16 @@ class validation::PandoraValidation : public art::EDAnalyzer {
         std::vector<float> fPCASecondaryRatio;
         std::vector<float> fPCATertiaryRatio;
         std::vector<float> fVertexDistance;
+        std::vector<float> fNuScore;
+        std::vector<float> fNuEigenRatioInSphere;
+        std::vector<float> fNuNFinalStatePfos;
+        std::vector<float> fNuNHitsTotal;
+        std::vector<float> fNuNSpacePointsInSphere;
+        std::vector<float> fNuWeightedDirZ;
+        std::vector<float> fCRFracHitsInLongestTrack;
+        std::vector<float> fCRLongestTrackDeflection;
+        std::vector<float> fCRLongestTrackDirY;
+        std::vector<float> fCRNHitsMax;
 };
 
 
@@ -100,61 +117,71 @@ void validation::PandoraValidation::analyze(art::Event const& e)
     if (!pfpHandle.isValid())
         return;
     art::FindManyP<larpandoraobj::PFParticleMetadata> pfpMetadataAssoc(pfpHandle, e, fPFParticleLabel);
+    art::FindManyP<recob::Vertex> pfpVertexAssoc(pfpHandle, e, fPFParticleLabel);
 
     for (const art::Ptr<recob::Slice> &slice : sliceVector)
     {
         std::vector<art::Ptr<recob::PFParticle>> slicePFPs{slicePFPAssoc.at(slice.key())};
-        for (const art::Ptr<recob::PFParticle> &slicePFP : slicePFPs)
+        for (const art::Ptr<recob::PFParticle> &pfp : slicePFPs)
         {
-            const int pdg{std::abs(slicePFP->PdgCode())};
+            const int pdg{std::abs(pfp->PdgCode())};
             const bool isNeutrino{pdg == 12 || pdg ==14};
 
             if (isNeutrino)
+            {
+                std::vector<art::Ptr<recob::Vertex>> pfpVertices{pfpVertexAssoc.at(pfp.key())};
+                if (pfpVertices.size() == 1)
+                {
+                    art::Ptr<recob::Vertex> pfpVertex{pfpVertices.front()};
+                    fNuVertexX = pfpVertex->position().x();
+                    fNuVertexY = pfpVertex->position().y();
+                    fNuVertexZ = pfpVertex->position().z();
+                }
                 fIsNuSlice = true;
+            }
 
             fNPFParticle = slicePFPs.size();
-            for (const art::Ptr<recob::PFParticle> &pfp : slicePFPs)
-            {
                 auto metadataVec{pfpMetadataAssoc.at(pfp.key())};
 
-                if (!metadataVec.empty())
+            std::cout << "PDG code: " << pdg << std::endl;
+            if (!metadataVec.empty())
+            {
+                auto metadata{metadataVec.front()};
+                const auto &properties{metadata->GetPropertiesMap()};
+                for (const auto &[key, value] : properties)
+                    std::cout << "  " << key << ": " << value << std::endl;
+                this->AddPropertyToVector(properties, "TrackScore", fTrackScore);
+                this->AddPropertyToVector(properties, "LArConeChargeFeatureTool_Concentration", fConeChargeConcentration);
+                this->AddPropertyToVector(properties, "LArConeChargeFeatureTool_Conicalness", fConeChargeConicalness);
+                this->AddPropertyToVector(properties, "LArConeChargeFeatureTool_HaloTotalRatio", fConeChargeHaloTotRatio);
+                this->AddPropertyToVector(properties, "LArThreeDChargeFeatureTool_EndFraction", fChargeEndFrac);
+                this->AddPropertyToVector(properties, "LArThreeDChargeFeatureTool_FractionalSpread", fChargeFracSpread);
+                this->AddPropertyToVector(properties, "LArThreeDLinearFitFeatureTool_DiffStraightLineM", fLinearDiffStraightLineMean);
+                this->AddPropertyToVector(properties, "LArThreeDLinearFitFeatureTool_Length", fLinearLength);
+                this->AddPropertyToVector(properties, "LArThreeDLinearFitFeatureTool_MaxFitGapLength", fLinearMaxFitGapLength);
+                this->AddPropertyToVector(properties, "LArThreeDLinearFitFeatureTool_SlidingLinearFitRMS", fLinearFitRMS);
+                this->AddPropertyToVector(properties, "LArThreeDOpeningAngleFeatureTool_AngleDiff", fOpenAngleDiff);
+                this->AddPropertyToVector(properties,"LArThreeDPCAFeatureTool_SecondaryPCARatio", fPCASecondaryRatio);
+                this->AddPropertyToVector(properties, "LArThreeDPCAFeatureTool_TertiaryPCARatio", fPCATertiaryRatio);
+                this->AddPropertyToVector(properties, "LArThreeDVertexDistanceFeatureTool_VertexDistance", fVertexDistance);
+
+                if (isNeutrino)
                 {
-                    auto metadata{metadataVec.front()};
-                    const auto &properties{metadata->GetPropertiesMap()};
-                    if (properties.find("TrackScore") != properties.end())
-                        fTrackScore.emplace_back(properties.at("TrackScore"));
-                    if (properties.find("LArConeChargeFeatureTool_Concentration") != properties.end())
-                        fConeChargeConcentration.emplace_back(properties.at("LArConeChargeFeatureTool_Concentration"));
-                    if (properties.find("LArConeChargeFeatureTool_Conicalness") != properties.end())
-                        fConeChargeConicalness.emplace_back(properties.at("LArConeChargeFeatureTool_Conicalness"));
-                    if (properties.find("LArConeChargeFeatureTool_HaloTotalRatio") != properties.end())
-                        fConeChargeHaloTotRatio.emplace_back(properties.at("LArConeChargeFeatureTool_HaloTotalRatio"));
-                    if (properties.find("LArThreeDChargeFeatureTool_EndFraction") != properties.end())
-                        fChargeEndFrac.emplace_back(properties.at("LArThreeDChargeFeatureTool_EndFraction"));
-                    if (properties.find("LArThreeDChargeFeatureTool_FractionalSpread") != properties.end())
-                        fChargeFracSpread.emplace_back(properties.at("LArThreeDChargeFeatureTool_FractionalSpread"));
-                    if (properties.find("LArThreeDLinearFitFeatureTool_DiffStraightLineMean") != properties.end())
-                        fLinearDiffStraightLineMean.emplace_back(properties.at("LArThreeDLinearFitFeatureTool_DiffStraightLineMean"));
-                    if (properties.find("LArThreeDLinearFitFeatureTool_Length") != properties.end())
-                        fLinearLength.emplace_back(properties.at("LArThreeDLinearFitFeatureTool_Length"));
-                    if (properties.find("LArThreeDLinearFitFeatureTool_MaxFitGapLength") != properties.end())
-                        fLinearMaxFitGapLength.emplace_back(properties.at("LArThreeDLinearFitFeatureTool_MaxFitGapLength"));
-                    if (properties.find("LArThreeDLinearFitFeatureTool_SlidingLinearFitRMS") != properties.end())
-                        fLinearFitRMS.emplace_back(properties.at("LArThreeDLinearFitFeatureTool_SlidingLinearFitRMS"));
-                    if (properties.find("LArThreeDOpeningAngleFeatureTool_AngleDiff") != properties.end())
-                        fOpenAngleDiff.emplace_back(properties.at("LArThreeDOpeningAngleFeatureTool_AngleDiff"));
-                    if (properties.find("LArThreeDPCAFeatureTool_SecondaryPCARatio") != properties.end())
-                        fPCASecondaryRatio.emplace_back(properties.at("LArThreeDPCAFeatureTool_SecondaryPCARatio"));
-                    if (properties.find("LArThreeDPCAFeatureTool_TertiaryPCARatio") != properties.end())
-                        fPCATertiaryRatio.emplace_back(properties.at("LArThreeDPCAFeatureTool_TertiaryPCARatio"));
-                    if (properties.find("LArThreeDVertexDistanceFeatureTool_VertexDistance") != properties.end())
-                        fVertexDistance.emplace_back(properties.at("LArThreeDVertexDistanceFeatureTool_VertexDistance"));
+                    this->AddPropertyToVector(properties, "NuScore", fNuScore);
+                    this->AddPropertyToVector(properties, "NuEigenRatioInSphere", fNuEigenRatioInSphere);
+                    this->AddPropertyToVector(properties, "NuNFinalStatePfos", fNuNFinalStatePfos);
+                    this->AddPropertyToVector(properties, "NuNHitsTotal", fNuNHitsTotal);
+                    this->AddPropertyToVector(properties, "NuNSpacePointsInSphere", fNuNSpacePointsInSphere);
+                    this->AddPropertyToVector(properties, "NuWeightedDirZ", fNuWeightedDirZ);
+                    this->AddPropertyToVector(properties, "CRFracHitsInLongestTrack", fCRFracHitsInLongestTrack);
+                    this->AddPropertyToVector(properties, "CRLongestTrackDeflection", fCRLongestTrackDeflection);
+                    this->AddPropertyToVector(properties, "CRLongestTrackDirY", fCRLongestTrackDirY);
+                    this->AddPropertyToVector(properties, "CRNHitsMax", fCRNHitsMax);
                 }
             }
         }
+        fTree->Fill();
     }
-
-    fTree->Fill();
 }
 
 void validation::PandoraValidation::beginJob()
@@ -164,6 +191,9 @@ void validation::PandoraValidation::beginJob()
 
     fTree->Branch("eventID", &fEventID);
     fTree->Branch("isNuSlice", &fIsNuSlice);
+    fTree->Branch("fNuVertexX", &fNuVertexX);
+    fTree->Branch("fNuVertexY", &fNuVertexY);
+    fTree->Branch("fNuVertexZ", &fNuVertexZ);
     fTree->Branch("nPFParticle", &fNPFParticle);
     fTree->Branch("fTrackScore", &fTrackScore);
     fTree->Branch("fConeChargeConcentratio", &fConeChargeConcentration);
@@ -179,6 +209,16 @@ void validation::PandoraValidation::beginJob()
     fTree->Branch("fPCASecondaryRatio", &fPCASecondaryRatio);
     fTree->Branch("fPCATertiaryRatio", &fPCATertiaryRatio);
     fTree->Branch("fVertexDistance", &fVertexDistance);
+    fTree->Branch("fNuScore", &fNuScore);
+    fTree->Branch("fNuEigenRatioInSphere", &fNuEigenRatioInSphere);
+    fTree->Branch("fNuNFinalStatePfos", &fNuNFinalStatePfos);
+    fTree->Branch("fNuNHitsTotal", &fNuNHitsTotal);
+    fTree->Branch("fNuNSpacePointsInSphere", &fNuNSpacePointsInSphere);
+    fTree->Branch("fNuWeightedDirZ", &fNuWeightedDirZ);
+    fTree->Branch("fCRFracHitsInLongestTrack", &fCRFracHitsInLongestTrack);
+    fTree->Branch("fCRLongestTrackDeflection", &fCRLongestTrackDeflection);
+    fTree->Branch("fCRLongestTrackDirY", &fCRLongestTrackDirY);
+    fTree->Branch("fCRNHitsMax", &fCRNHitsMax);
 }
 
 void validation::PandoraValidation::endJob()
@@ -190,6 +230,9 @@ void validation::PandoraValidation::Reset()
 {
     fNPFParticle = 0;
     fIsNuSlice = false;
+    fNuVertexX = -999;
+    fNuVertexY = -999;
+    fNuVertexZ = -999;
     fTrackScore.clear();
     fConeChargeConcentration.clear();
     fConeChargeConicalness.clear();
@@ -204,6 +247,23 @@ void validation::PandoraValidation::Reset()
     fPCASecondaryRatio.clear();
     fPCATertiaryRatio.clear();
     fVertexDistance.clear();
+    fNuScore.clear();
+    fNuEigenRatioInSphere.clear();
+    fNuNFinalStatePfos.clear();
+    fNuNHitsTotal.clear();
+    fNuNSpacePointsInSphere.clear();
+    fNuWeightedDirZ.clear();
+    fCRFracHitsInLongestTrack.clear();
+    fCRLongestTrackDeflection.clear();
+    fCRLongestTrackDirY.clear();
+    fCRNHitsMax.clear();
+}
+
+template<typename T>
+void validation::PandoraValidation::AddPropertyToVector(const larpandoraobj::PFParticleMetadata::PropertiesMap &properties, const std::string &key, std::vector<T> vector)
+{
+    if (properties.find(key) != properties.end())
+        vector.emplace_back(properties.at(key));
 }
 
 DEFINE_ART_MODULE(validation::PandoraValidation)
